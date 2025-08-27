37bf2299f8e0e7911b08c84da05a5f9f165e1e8f
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-27 08:36:00 +0800

    feat: implement custom error in ResponseFormatter.ts

--- src/presentation/utils/ResponseFormatter.ts
+++ src/presentation/utils/ResponseFormatter.ts
@@ -1,36 +1,54 @@
-export interface SuccessResponse<T>{
-    success: true;
-    data: T;
-    message: string;
+import { ValidationError } from "elysia";
+import { DatabaseError, LogicError, NotFoundError } from "../../domain/errors/customErrors";
+
+export interface SuccessResponse<T> {
+  success: true;
+  data: T;
+  message: string;
 }
 
-export interface ErrorResponse{
-    success: false;
-    error: string;
-    details?: string;
+export interface ErrorResponse {
+  success: false;
+  error: string;
+  details?: string;
 }
 
 export type ApiResponse<T> = SuccessResponse<T> | ErrorResponse;
 
 export class ResponseFormatter {
-    static success<T>(data: T, message: string): SuccessResponse<T> {
-        return {
-            success: true,
-            data,
-            message,
-        };
+  static success<T>(data: T, message: string): SuccessResponse<T> {
+    return {
+      success: true,
+      data,
+      message,
+    };
+  }
+
+  static handleError(error: string, details?: string): ErrorResponse {
+    return {
+      success: false,
+      error,
+      details,
+    };
+  }
+
+  static error(error: unknown, defaultMessage: string): ErrorResponse {
+    if (error instanceof ValidationError) {
+      return this.handleError("Validation Error", error.message);
+    }
+    if (error instanceof NotFoundError) {
+      return this.handleError("Not Found", error.message);
     }
 
-    static handlerError(error: string, details?: string): ErrorResponse {
-        return {
-            success: false,
-            error,
-            details,
-        };
+    if (error instanceof LogicError) {
+      return this.handleError("Logic Error", error.message);
     }
 
-    static error(error: unknown, defaultMessage: string): ErrorResponse {
-        const details = error instanceof Error ? error.message : "Unknown error";
-        return this.handlerError(defaultMessage, details);
+    if (error instanceof DatabaseError) {
+      return this.handleError("Database Error", error.message);
     }
+
+    const details = error instanceof Error ? error.message : "Unknown error";
+    return this.handleError(defaultMessage, details);
+  }
 }
