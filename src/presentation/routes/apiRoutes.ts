081e067336e8598852fef2c892359c0602ae35e5
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-27 10:35:20 +0800

    fix: route problem path

--- src/presentation/routes/apiRoutes.ts
+++ src/presentation/routes/apiRoutes.ts
@@ -1,8 +1,8 @@
 import Elysia from "elysia";
 import type { FolderController } from "../controllers/FolderController";
-import { createV1Routes } from "./v1routes";
+import { createV1Routes } from "./v1Routes";
 
 export function createApiRoutes(folderController: FolderController) {
-    return new Elysia({prefix: 'v1'})
+    return new Elysia({prefix: '/api'})
         .use(createV1Routes(folderController));
 }
--- src/presentation/routes/v1Routes.ts
+++ src/presentation/routes/v1Routes.ts
@@ -3,6 +3,6 @@ import type { FolderController } from "../controllers/FolderController";
 import { createFolderRoutes } from "./folderRoutes";
 
 export function createV1Routes(folderController: FolderController) {
-    return new Elysia({prefix: 'v1'})
+    return new Elysia({prefix: '/v1'})
         .use(createFolderRoutes(folderController));
 }
