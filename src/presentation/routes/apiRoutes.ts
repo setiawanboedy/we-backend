d56456aabed920ab5f139c70ec1516ad000267a3
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-28 11:34:00 +0800

    feat: create fileroutes for api endopoints

--- src/presentation/routes/apiRoutes.ts
+++ src/presentation/routes/apiRoutes.ts
@@ -1,8 +1,9 @@
 import Elysia from "elysia";
 import type { FolderController } from "../controllers/FolderController";
+import type { FileController } from "../controllers/FileController";
 import { createV1Routes } from "./v1Routes";
 
-export function createApiRoutes(folderController: FolderController) {
+export function createApiRoutes(folderController: FolderController, fileController: FileController) {
     return new Elysia({prefix: '/api'})
-        .use(createV1Routes(folderController));
+        .use(createV1Routes(folderController, fileController));
 }
--- src/presentation/routes/v1Routes.ts
+++ src/presentation/routes/v1Routes.ts
@@ -1,8 +1,11 @@
 import Elysia from "elysia";
 import type { FolderController } from "../controllers/FolderController";
+import type { FileController } from "../controllers/FileController";
 import { createFolderRoutes } from "./folderRoutes";
+import { createFileRoutes } from "./fileRoutes";
 
-export function createV1Routes(folderController: FolderController) {
+export function createV1Routes(folderController: FolderController, fileController: FileController) {
     return new Elysia({prefix: '/v1'})
-        .use(createFolderRoutes(folderController));
+        .use(createFolderRoutes(folderController))
+        .use(createFileRoutes(fileController));
 }
