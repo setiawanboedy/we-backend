a4feb61a5ff31362dc2ed4e2951bbcc98408b46d
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-28 11:29:43 +0800

    feat: add domain entity file.ts

---
+++ src/domain/entities/File.ts
@@ -0,0 +1,26 @@
+export interface FileEntity {
+  id: string;
+  name: string;
+  path: string;
+  size?: number | null;
+  mimeType?: string | null;
+  folderId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface CreateFileData {
+  name: string;
+  path: string;
+  size?: number;
+  mimeType?: string;
+  folderId: string;
+}
+
+export interface UpdateFileData {
+  name?: string;
+  path?: string;
+  size?: number;
+  mimeType?: string;
+  folderId?: string;
+}
