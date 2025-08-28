1cfdc94bb185d8c19f4a01f9ddf35e2447837d1e
setiawanboedy <gonisetiawan0@gmail.com>
2025-08-28 11:36:54 +0800

    feat: update injection file

--- src/di/Injection.ts
+++ src/di/Injection.ts
@@ -1,19 +1,27 @@
 import type { IFolderService } from "../application/interfaces/IFolderService";
+import type { IFileService } from "../application/interfaces/IFileService";
 import { FolderService } from "../application/services/FolderService";
+import { FileService } from "../application/services/FileService";
 import type { FolderRepository } from "../domain/repositories/FolderRepository";
+import type { FileRepository } from "../domain/repositories/FileRepository";
 import type { ILogger } from "../infrastructure/logging/ILogger";
 import { logger } from "../infrastructure/logging/LoggerFactory";
 import { RequestLogger } from "../infrastructure/logging/RequestLogger";
 import { PrismaFolderRepository } from "../infrastructure/repositories/PrismaFolderRepository";
+import { PrismaFileRepository } from "../infrastructure/repositories/PrismaFileRepository";
 import { BaseController } from "../presentation/controllers/BaseController";
 import { FolderController } from "../presentation/controllers/FolderController";
+import { FileController } from "../presentation/controllers/FileController";
 
 export class Injection {
   private static instance: Injection;
 
   private _folderRepository: FolderRepository;
+  private _fileRepository: FileRepository;
   private _folderService: IFolderService;
+  private _fileService: IFileService;
   private _folderController: FolderController;
+  private _fileController: FileController;
   private _baseController: BaseController;
   private _logger: ILogger;
   private _requestLogger: RequestLogger;
@@ -22,8 +30,11 @@ export class Injection {
     this._logger = logger;
     this._requestLogger = new RequestLogger(this._logger);
     this._folderRepository = new PrismaFolderRepository();
+    this._fileRepository = new PrismaFileRepository();
     this._folderService = new FolderService(this._folderRepository, this._logger);
+    this._fileService = new FileService(this._fileRepository, this._folderRepository, this._logger);
     this._folderController = new FolderController(this.folderService);
+    this._fileController = new FileController(this._fileService);
     this._baseController = new BaseController();
   }
 
@@ -44,6 +55,18 @@ export class Injection {
 
   public get folderController(): FolderController {
     return this._folderController;
+  }
+
+  public get fileRepository(): FileRepository {
+    return this._fileRepository;
+  }
+
+  public get fileService(): IFileService {
+    return this._fileService;
+  }
+
+  public get fileController(): FileController {
+    return this._fileController;
   }
 
   public get baseController(): BaseController {
